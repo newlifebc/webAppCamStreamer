0fe470be119e2480199dc834608294ce595209e2
Swarts, Matthew E <strontium5@gmail.com>
2019-07-13 11:24:26 -0400

    changed ports to be non standard; redirection to https goes to 443 only

--- app.js
+++ app.js
@@ -9,15 +9,15 @@ var options = {
 };
 
 var port = process.env.PORT || 80;
-
+var portSSL = 4430;
 
 //https://medium.com/@nileshsingh/everything-about-creating-an-https-server-using-node-js-2fc5c48a8d4e
 const express = require('express');
 const app = express();
-const forceSsl = require('express-force-ssl');
-app.use(forceSsl);
-const serverSSL = require('https').createServer(options,app).listen(443);
-const server = require('http').createServer(app).listen(80);
+const forceSSL = require('express-force-ssl');
+app.use(forceSSL);
+const serverSSL = require('https').createServer(options,app).listen(portSSL);
+const server = require('http').createServer(app).listen(port);
 
 const io = require('socket.io')(server);
 
@@ -25,4 +25,3 @@ app.use(express.static(__dirname + '/public'));
 
 app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
 app.use('/', express.static(__dirname + '/node_modules/bootstrap/dist')); // redirect bootstrap JS, CSS, and fonts
-
