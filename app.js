43ddfc480645a8ec187c8d718610c1cab57a670d
Swarts, Matthew E <strontium5@gmail.com>
2019-07-13 10:56:32 -0400

    added some webrtc stuff

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const fs = require('fs');
 const path = require('path');
 const express = require('express');
 const app = express();
-const server = require('http').createServer(app);
+const serverSSL = require('https').createServer(app).listen(443);
+const server = require('http').createServer(app).listen(80);
 const io = require('socket.io')(server);
 
 app.use(express.static(__dirname + '/public')); 
@@ -10,6 +11,5 @@ app.use(express.static(__dirname + '/public'));
 app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
 app.use('/', express.static(__dirname + '/node_modules/bootstrap/dist')); // redirect bootstrap JS, CSS, and fonts
 
-process.env.PORT = 80;
 var port = process.env.PORT || 80;
 server.listen(port);
