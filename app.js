bc9b946d38a32d3257c9db811b6128ab1a9ddb4e
Swarts, Matthew E <strontium5@gmail.com>
2019-07-13 10:33:39 -0400

    initial setup with webserver

---
+++ app.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+app.use(express.static(__dirname + '/public')); 
+
+app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
+app.use('/', express.static(__dirname + '/node_modules/bootstrap/dist')); // redirect bootstrap JS, CSS, and fonts
+
+process.env.PORT = 80;
+var port = process.env.PORT || 80;
+server.listen(port);
