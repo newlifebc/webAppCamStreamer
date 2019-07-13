1f7de5a6e676d95bedbde2f934a70947d6eeacc2
Swarts, Matthew E <strontium5@gmail.com>
2019-07-13 13:31:23 -0400

    added webrtc-adapter

--- app.js
+++ app.js
@@ -25,9 +25,10 @@ app.set('forceSSLOptions', {
 const serverSSL = require('https').createServer(options,app).listen(portSSL);
 const server = require('http').createServer(app).listen(port);
 
-const io = require('socket.io')(server);
+const io = require('socket.io')(serverSSL);
 
 app.use(express.static(__dirname + '/public')); 
 
 app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
 app.use('/', express.static(__dirname + '/node_modules/bootstrap/dist')); // redirect bootstrap JS, CSS, and fonts
+app.use('/js', express.static(__dirname + '/node_modules/webrtc-adapter/out')); // redirect webrtc adapter JS
